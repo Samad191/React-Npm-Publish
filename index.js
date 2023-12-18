9ec4d63c339d013207d9d354cadcb2ab11a2e5f0
Samad191 <samad13354@gmail.com>
2023-12-18 16:31:32 +0500

    feat: configurated npm packgae

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = ExampleReactNpmModule;
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function ExampleReactNpmModule() {
+  return /*#__PURE__*/_react["default"].createElement("div", null, /*#__PURE__*/_react["default"].createElement("h1", null, "Hello from npm :)"));
+}
