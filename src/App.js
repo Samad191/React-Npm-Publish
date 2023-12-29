2077ca5716544bda43a24f4483a7cc5e65fe3686
Samad191 <samad13354@gmail.com>
2023-12-30 02:00:48 +0500

    feat: npm package int

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ExampleReactNpmModule from 'samad-uni-test-widget';
+// import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react'
+
+// import { WagmiConfig } from 'wagmi'
+// import { goerli, mainnet } from 'viem/chains'
+// import ConnectButton from './hooks/connectWallet';
+// import { useWeb3ModalState } from '@web3modal/wagmi/react'
+// import { useAccount } from 'wagmi';
 
 function App() {
+
+  // const projectId = 'ec34b7ce08621ea43343eaf71e71e86b';
+  // // createWeb3Modal()
+  // const metadata = {
+  //   name: 'Web3Modal',
+  //   description: 'Web3Modal Example',
+  //   url: 'https://web3modal.com',
+  //   icons: ['https://avatars.githubusercontent.com/u/37784886']
+  // }
+
+  // const chains = [mainnet, goerli]
+
+  // const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata })
+  // createWeb3Modal({ wagmiConfig, projectId, chains })
+  // const { open, selectedNetworkId } = useWeb3ModalState()
+
+  // return (
+  //   <WagmiConfig config={wagmiConfig} >
+  //     <div className="App">
+  //       <ExampleReactNpmModule backgroundColor='#D0D0D0' height='10rem' />
+  //       <ConnectButton />
+  //       <button onClick={() => {console.log('state of wallet', open, selectedNetworkId)}} >Get wallet details</button>
+  //       <h4>network id: {selectedNetworkId}</h4>
+  //     </div>
+  //   </WagmiConfig>
+  // );
+
   return (
-    <div className="App">
-    <ExampleReactNpmModule />
-    </div>
+      <div className="App">
+        <ExampleReactNpmModule backgroundColor='#D0D0D0' height='10rem' />
+      </div>
   );
 }
 
---
+++ src/hooks/connectWallet.js
@@ -0,0 +1,18 @@
+// import { useWeb3Modal } from '@web3modal/wagmi/react'
+// import { useAccount } from 'wagmi';
+
+// export default function ConnectButton() {
+//   // 4. Use modal hook
+//   const { open } = useWeb3Modal()
+//    const { address } = useAccount()
+//   console.log('account', address)
+ 
+
+//   return (
+//     <>
+//       <button onClick={() => open()}>Open Connect Modal</button>
+//       <button onClick={() => open({ view: 'Networks' })}>Open Network Modal</button>
+//       <h4>Address: {address}</h4>
+//     </>
+//   )
+// }
