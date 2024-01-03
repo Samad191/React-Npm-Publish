962d06b88047158b18328094c6c93e61f8e2825b
Samad191 <samad13354@gmail.com>
2024-01-03 18:50:49 +0500

    feat: package working with use-wallet-client

--- src/App.js
+++ src/App.js
@@ -1,48 +1,67 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ExampleReactNpmModule from 'samad-uni-test-widget';
-// import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react'
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react'
 
-// import { WagmiConfig } from 'wagmi'
-// import { goerli, mainnet } from 'viem/chains'
-// import ConnectButton from './hooks/connectWallet';
-// import { useWeb3ModalState } from '@web3modal/wagmi/react'
-// import { useAccount } from 'wagmi';
+import { WagmiConfig, usePublicClient } from 'wagmi'
+import { goerli, mainnet } from 'viem/chains'
+import ConnectButton from './hooks/connectWallet';
+import { useWeb3ModalState } from '@web3modal/wagmi/react'
+import { useAccount } from 'wagmi';
 
 function App() {
 
-  // const projectId = 'ec34b7ce08621ea43343eaf71e71e86b';
-  // // createWeb3Modal()
-  // const metadata = {
-  //   name: 'Web3Modal',
-  //   description: 'Web3Modal Example',
-  //   url: 'https://web3modal.com',
-  //   icons: ['https://avatars.githubusercontent.com/u/37784886']
-  // }
 
-  // const chains = [mainnet, goerli]
+  const projectId = 'ec34b7ce08621ea43343eaf71e71e86b';
+  const metadata = {
+    name: 'Web3Modal',
+    description: 'Web3Modal Example',
+    url: 'https://web3modal.com',
+    icons: ['https://avatars.githubusercontent.com/u/37784886']
+  }
 
-  // const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata })
-  // createWeb3Modal({ wagmiConfig, projectId, chains })
-  // const { open, selectedNetworkId } = useWeb3ModalState()
+  const chains = [mainnet, goerli]
 
-  // return (
-  //   <WagmiConfig config={wagmiConfig} >
-  //     <div className="App">
-  //       <ExampleReactNpmModule backgroundColor='#D0D0D0' height='10rem' />
-  //       <ConnectButton />
-  //       <button onClick={() => {console.log('state of wallet', open, selectedNetworkId)}} >Get wallet details</button>
-  //       <h4>network id: {selectedNetworkId}</h4>
-  //     </div>
-  //   </WagmiConfig>
-  // );
+  const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata })
+  createWeb3Modal({ wagmiConfig, projectId, chains })
+  const { open, selectedNetworkId } = useWeb3ModalState()
+
+  // const { address, connector } = useAccount()
+
+  console.log('network id', selectedNetworkId)
+
+  const provider = usePublicClient({ chainId: selectedNetworkId });
+  console.log('provider', provider);
+
+  console.log('app')
+
+  useEffect(() => {
+    console.log('use effe')
+  }, [])
+
+
+
 
   return (
+    <WagmiConfig config={wagmiConfig} >
+
       <div className="App">
-        <ExampleReactNpmModule backgroundColor='#D0D0D0' height='10rem' />
+        {/* <ExampleReactNpmModule backgroundColor='#D0D0D0' height='10rem' ethereumProvider={provider} /> */}
+        <h1>React app</h1>
+        <ConnectButton />
+        {/* <button onClick={() => {console.log('state of wallet', address, selectedNetworkId)}} >Get wallet details</button> */}
+        {/* <h4>network id: {selectedNetworkId}</h4> */}
       </div>
+    </WagmiConfig>
   );
 }
+
+// return (
+//     <div className="App">
+//       <ExampleReactNpmModule backgroundColor='#D0D0D0' height='10rem' />
+//     </div>
+// );
+// }
 
 export default App;
--- src/hooks/connectWallet.js
+++ src/hooks/connectWallet.js
@@ -1,18 +1,33 @@
-// import { useWeb3Modal } from '@web3modal/wagmi/react'
-// import { useAccount } from 'wagmi';
+import { useWeb3Modal } from '@web3modal/wagmi/react'
+import { useAccount, usePublicClient, useWalletClient } from 'wagmi';
+import { useWeb3ModalState } from '@web3modal/wagmi/react'
 
-// export default function ConnectButton() {
-//   // 4. Use modal hook
-//   const { open } = useWeb3Modal()
-//    const { address } = useAccount()
-//   console.log('account', address)
- 
+import ExampleReactNpmModule from 'samad-uni-test-widget';
 
-//   return (
-//     <>
-//       <button onClick={() => open()}>Open Connect Modal</button>
-//       <button onClick={() => open({ view: 'Networks' })}>Open Network Modal</button>
-//       <h4>Address: {address}</h4>
-//     </>
-//   )
-// }
+export default function ConnectButton() {
+    // 4. Use modal hook
+    const { open } = useWeb3Modal()
+    const { address, connector } = useAccount()
+    const { selectedNetworkId } = useWeb3ModalState()
+
+    // const publicClient = usePublicClient({ chainId: 5 });
+    const { data } = useWalletClient()
+
+    console.log('connect provider frontend', data);
+    console.log('connect account', address)
+
+
+    return (
+        <>
+            <button onClick={() => open()}>Open Connect Modal</button>
+            <button onClick={() => open({ view: 'Networks' })}>Open Network Modal</button>
+            <h4>Address: {address}</h4>
+            <button
+                onClick={() => {
+                    console.log('details', address, selectedNetworkId)
+                }}
+            >Get details</button>
+            <ExampleReactNpmModule address={address} backgroundColor='#D0D0D0' height='10rem' publicClient={data} />
+        </>
+    )
+}
